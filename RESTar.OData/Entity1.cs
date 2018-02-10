33926af5884ffd3339ab1221b1b62a41f3b55661
Erik von Krusenstierna <erik.vk@me.com>
2018-02-10 12:49:00 +0100

    Add project files.

---
+++ RESTar.OData/Entity1.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+using Starcounter;
+
+namespace RESTar.OData
+{
+
+    [Database]
+    public class Entity1
+    {
+        public string Field1;
+    }
+}
---
+++ RESTar.OData/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Starcounter.Internal;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RESTar.OData")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Starcounter")]
+[assembly: AssemblyProduct("RESTar.OData")]
+[assembly: AssemblyCopyright("Copyright © Starcounter 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("97346845-94BB-4D8E-AC10-2B62CA324867")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Assures the current assembly has a reference to the Starcounter
+// assembly. A reference to Starcounter is currently required for
+// Starcounter to detect that an assembly should be hosted.
+[assembly: StarcounterAssembly()]
